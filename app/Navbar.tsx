7448659137e5533eb8335a6e90c9a2a584a09075
Coder030 <garg2012030@gmail.com>
2024-02-27 17:24:43 +0530

    This is just to check if zed works fine for github

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,30 +1,42 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import styles from "./navbar.module.css"
-import Image from 'next/image';
-import logo from "./logo.png"
-import { useRouter } from 'next/navigation';
-
+import Link from "next/link";
+import styles from "./navbar.module.css";
+import Image from "next/image";
+import logo from "./logo.png";
+import { useRouter } from "next/navigation";
 
 function Navbar() {
-  const router = useRouter()
+  const router = useRouter();
+  const nothing = "";
   const han = () => {
-    router.push("https://github.com/coder030")
-  }
+    router.push("https://github.com/coder030");
+  };
   return (
     <div className={styles.nav}>
-      {// eslint-disable-next-line jsx-a11y/alt-text
-      <Image onClick={han} className={styles.img} src={logo} alt={""}></Image>}
+      {
+        // eslint-disable-next-line jsx-a11y/alt-text
+        <Image onClick={han} className={styles.img} src={logo} alt={""}></Image>
+      }
       <div className={styles.nav2}>
-        <Link className={styles.link} href="/">Home</Link>
-        <Link className={styles.link} href="/api/post">Create Jobs</Link>
-        <Link className={styles.link} href="/api/look">Find Jobs</Link>
-        <Link className={styles.link} href="/log">Log in</Link>
-        <Link className={styles.link} href="/sign">Sign up</Link>
+        <Link className={styles.link} href="/">
+          Home
+        </Link>
+        <Link className={styles.link} href="/api/post">
+          Create Jobs
+        </Link>
+        <Link className={styles.link} href="/api/look">
+          Find Jobs
+        </Link>
+        <Link className={styles.link} href="/log">
+          Log in
+        </Link>
+        <Link className={styles.link} href="/sign">
+          Sign up
+        </Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
